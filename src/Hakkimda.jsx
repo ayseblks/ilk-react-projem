06c6d0ba92f799ed2322165b22f472094f361f40
Ayşe İlbuğa <ayseblks04@gmail.com>
2026-07-30 02:42:34 +0300

    metin duzeltme

--- src/Hakkimda.jsx
+++ src/Hakkimda.jsx
@@ -3,7 +3,7 @@ function Hakkimda() {
         <div className="hakkimda-kutu">
             <h1>Hakkımda</h1>
             <p>Web tasarımı ve React öğreniyorum</p>
-            <p>Web tasarımına ara verip KPSS çalışmaya geçiyorum</p>
+            <p>Web tasarımına ara verip KPSS çalışağım</p>
             <p>KPSS bitince kaldığım yerden devam edeceğim</p>
         </div>
     );
